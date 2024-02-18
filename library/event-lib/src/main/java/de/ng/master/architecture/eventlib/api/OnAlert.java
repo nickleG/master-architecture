d29eb05f7a694f1bc3f87bf78de5e6af62547c18
Niklas Gieretz <niklas.gieretz@hdi.de>
2024-02-18 11:27:33 +0100

    fix: added request body to interfaces

--- library/event-lib/src/main/java/de/ng/master/architecture/eventlib/api/OnAlert.java
+++ library/event-lib/src/main/java/de/ng/master/architecture/eventlib/api/OnAlert.java
@@ -3,9 +3,10 @@ package de.ng.master.architecture.eventlib.api;
 import de.ng.master.architecture.eventlib.events.Alert;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface OnAlert {
 
   @PostMapping(path = "/subscription/Alert", consumes = "application/json")
-  ResponseEntity<Void> onAlert(Alert message);
+  ResponseEntity<Void> onAlert(@RequestBody Alert message);
 }
